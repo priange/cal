65988f81181a590ac759aefdadb388e8fde0b6c2
priange <ityakup@gmail.com>
2023-05-08 00:20:46 +0300

    commit

--- app/src/main/java/com/example/usercalculator/MainActivity.kt
+++ app/src/main/java/com/example/usercalculator/MainActivity.kt
@@ -1,61 +1,63 @@
 package com.example.usercalculator
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity(){
-    add()
-    substract()
-    multiply()
-    divide()
-}
-fun addingNumbers() {
-    var number1 = findViewById<EditText>(R.id.number1)
-    var number2 = findViewById<EditText>(R.id.number2)
-    var button = findViewById<Button>(R.id.btn_add)
-    button.setOnClickListener {
-        var result1 = number1.text.toString().toInt()
-        var result2 = number2.text.toString().toInt()
-        var getResult = number1 + number22
-        Toast.makeText(this, "$getResult", Toast.LENGTH_SHORT).show()
-    }
-}
-fun substractNumbers() {
-    var num1 = findViewById<EditText>(R.id.number1)
-    var num2 = findViewById<EditText>(R.id.number2)
-    var button = findViewById<Button>(R.id.btn_subtract)
-    button.setOnClickListener {
-        var number1 = num1.text.toString().toInt()
-        var number2 = num2.text.toString().toInt()
-        var getResult = number1 - number2
-        Toast.makeText(this, "$getResult", Toast.LENGTH_SHORT).show()
-    }
-}
-fun multiplyNumbers() {
-    var number = findViewById<EditText>(R.id.number1)
-    var number2 = findViewById<EditText>(R.id.number2)
-    var button = findViewById<Button>(R.id.btn_multiply)
-    button.setOnClickListener {
-        var result1 = num1.text.toString().toInt()
-        var result2 = num2.text.toString().toInt()
-        var getResult = result1 * result2
-        Toast.makeText(this, "$getResult", Toast.LENGTH_SHORT).show()
-    }
-}
-fun divideNumbers() {
-    var num1 = findViewById<EditText>(R.id.number1)
-    var num2 = findViewById<EditText>(R.id.number2)
-    var button = findViewById<Button>(R.id.btn_div)
-    button.setOnClickListener {
-        var result1 = num1.text.toString().toInt()
-        var result2 = num2.text.toString().toInt()
-        var getResult = result1 / result2
-        Toast.makeText(this, "$getResult", Toast.LENGTH_SHORT).show()
-    }
-}
+    lateinit var number1:EditText
+    lateinit var number2:EditText
+    lateinit var btn_add:Button
+    lateinit var btn_subtract:Button
+    lateinit var btn_multiply:Button
+    lateinit var btn_divide:Button
+    lateinit var tvresult:TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
+        number1=findViewById(R.id.number1)
+        number2=findViewById(R.id.number2)
+        btn_add=findViewById(R.id.btn_add)
+        btn_subtract=findViewById(R.id.btn_subtract)
+        btn_multiply=findViewById(R.id.btn_multiply)
+        tvresult=findViewById(R.id.tvresult)
+        btn_divide=findViewById(R.id.btn_divide)
+        btn_add.setOnClickListener {
+            val numb1 = number1.text.toString().toDouble()
+            val numb2 = number2.text.toString().toDouble()
+            val result = numb1 + numb2
+            tvresult.text="Result"
+
+        }
+
+        btn_subtract.setOnClickListener {
+            val numb1 = number1.text.toString().toDouble()
+            val numb2 = number2.text.toString().toDouble()
+            val textView = numb1 - numb2
+            tvresult.text="Result"
+        }
+        btn_multiply.setOnClickListener {
+            val numb1 = number1.text.toString().toDouble()
+            val numb2 = number2.text.toString().toDouble()
+            val textView = numb1 * numb2
+            tvresult.text="Result"
+        }
+        btn_divide.setOnClickListener {
+            val numb1 = number1.text.toString().toDouble()
+            val numb2 = number2.text.toString().toDouble()
+            val textView = numb1 / numb2
+            tvresult.text="Result"
+
+        }
+
+
 
     }
 }
